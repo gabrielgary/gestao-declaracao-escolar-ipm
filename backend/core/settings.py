0066d490d909b2ba9d3816a3fb8070d4f14547f0
gabrielgary <gabrielpedroaurelio1@gmail.com>
2026-05-25 01:25:48 +0100

    fix: changed options deploy

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -23,9 +23,9 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-#ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost 127.0.0.1 192.168.216.221').split(' ')
-ALLOWED_HOSTS=['https://gestao-declaracao-escolar-ipm.onrender.com/admin']
-SITE_URL = os.environ.get('SITE_URL', 'https://ipm.onrender.com')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS','').split(' ')
+#ALLOWED_HOSTS=['https://gestao-declaracao-escolar-ipm.onrender.com/']
+SITE_URL = os.environ.get('SITE_URL', 'https://ipm-o29v.onrender.com/')
 
 # Segurança (Produção/HTTPS) - Controlado pelo .env
 USE_HTTPS = os.environ.get('USE_HTTPS', 'False') == 'True'
@@ -64,8 +64,8 @@ INSTALLED_APPS = [
 ]
 
 # CORS Configuration
-#CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://192.168.216.221:8000,http://localhost:3000,http://localhost:5173').split(',')
-CORS_ALLOW_ALL_ORIGINS=True
+CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', '').split(',')
+#CORS_ALLOW_ALL_ORIGINS=True
 CORS_ALLOW_CREDENTIALS = True
 
 MIDDLEWARE = [
